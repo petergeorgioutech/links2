0e104fb3531f7aa24591494d09f59fc7477e3bb2
Peter Georgiou <hello@petergeorgiou.tech>
2024-05-31 11:54:29 +0300

    update

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -7,12 +7,17 @@ const { data } = await useAsyncData('page-data', () => queryContent(route.path).
   <main>
     
     <ContentRenderer :value="data">
-      <p>{{ data.body }}</p> <br><br><br>
+      <h1>👇🏻 Title Field 👇🏻</h1>
       <h1>{{ data.title }}</h1> <br>
+      <h1>👇🏻 Custom Fields 👇🏻</h1>
       <p>{{ data.description }}</p> <br>
+      <h1>👇🏻 Image Field 👇🏻</h1>
+      <img :src="data.thumbnail" alt=""> <br>
+      <h1>👇🏻 Markdown WYSIWYG 👇🏻</h1>
       <ContentRendererMarkdown :value="data" />
     </ContentRenderer>
-
+    <br>
+    <h1>👇🏻 Source 👇🏻</h1>
     <pre>
       {{ data }}
     </pre>
