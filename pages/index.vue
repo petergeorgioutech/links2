3079098718dca2cf7f1eb00f8852328c81657d0b
Peter Georgiou <hello@petergeorgiou.tech>
2024-06-01 15:47:39 +0300

    updates

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,53 @@
   <div>
     <Header />
     <div class="p-8">
-      <Heading heading="Heading" />
-
-      <Paragraph text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto at laudantium explicabo labore recusandae voluptate, suscipit sit ipsa rerum porro, perspiciatis harum expedita sunt, eum odit ut! Ea, facere autem?" />
-
-      <AccordionGroup title="Accordion Group">
-        <AccordionItem image="https://placehold.co/600x600" text="Item in Group" link="https://google.com" />
-      </AccordionGroup>
-
-      <LinkButton title="Setup Gear" link="/setup" />
-
-      <Product />
+      <component
+        :is="getComponentType(component)"
+        v-for="(component, index) in homeData.components"
+        :key="`component-${index}`"
+        v-bind="component"
+      />
     </div>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+// import { useAsyncData } from '@nuxt/content'
+import Heading from '~/components/Heading.vue'
+import Paragraph from '~/components/Paragraph.vue'
+import AccordionGroup from '~/components/AccordionGroup.vue'
+import LinkButton from '~/components/LinkButton.vue'
+import Product from '~/components/Product.vue'
 
+// Initialize homeData with default structure
+const homeData = ref({ components: [] })
+
+// Fetch the homepage data using useAsyncData
+const { data, error } = await useAsyncData('homepage', () => queryContent('/homepage/home').findOne())
+
+// Assign data to homeData if available
+if (data.value) {
+  homeData.value = data.value
+} else {
+  console.error('Error fetching homepage data:', error.value)
+}
+
+// Function to get the component type
+const getComponentType = (component) => {
+  switch (component.type) {
+    case 'heading':
+      return Heading
+    case 'paragraph':
+      return Paragraph
+    case 'accordionGroup':
+      return AccordionGroup
+    case 'linkButton':
+      return LinkButton
+    case 'product':
+      return Product
+    default:
+      return null
+  }
+}
 </script>
