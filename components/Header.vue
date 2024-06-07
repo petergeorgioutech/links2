52eee05feacd4a47e1a7743d586e7865be9b13f9
Peter Georgiou <hello@petergeorgiou.tech>
2024-06-07 10:49:55 +0300

    added for collabs and media kit

--- components/Header.vue
+++ components/Header.vue
@@ -34,8 +34,18 @@
           <Icon color="white" width="35" icon="mage:email-opened" />
         </a>
       </nav>
+
       <a href="//secretlab.co/?rfsn=7910453.2764ccb&utm_source=refersion&utm_medium=affiliate&utm_campaign=7910453.2764ccb" target="_blank">
-        <img src="/secretlab_official_partner_white.svg" class="block mx-auto mt-8" alt="" width="350">
+        <img src="/secretlab_official_partner_white.svg" class="block mx-auto mt-8 mb-8" alt="" width="350">
+      </a>
+
+      <a href="mailto:hello@petergeorgiou.tech" class="flex justify-center items-center rounded-full bg-white px-4 py-2.5 text-sm font-semibold text-black shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-white hover:text-black w-1/2 mx-auto gap-2">
+        <span class="whitespace-nowrap">For Collaborations</span>
+        <Icon class="inline-block" width="20" icon="mage:email-opened" />
+      </a>
+      <a href="//beacons.ai/peterpandev/mediakit" target="_blank" class="flex justify-center items-center rounded-full bg-transparent px-4 py-2.5 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-white hover:text-black w-1/2 mx-auto gap-2 mt-4">
+        <span class="whitespace-nowrap">Media Kit</span>
+        <Icon class="inline-block" width="20" icon="mage:arrow-right-circle-fill" />
       </a>
     </div>
   </header>
